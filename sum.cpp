378763fa4e4424803dab5a755d3354b952ffdeca
Shekhar Aggarwal <50551462+shekharAggarwal@users.noreply.github.com>
2020-10-09 20:10:09 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sum(int x,int y){
+//write your code here
+}
+
+int main(){
+	int a,b;
+	cout<<"Enter first number:";
+	cin>>a;
+	cout<<"Enter second number:";
+	cin>>b;
+	cout<<"sum of "<<a<<" and "<<b<<" :"<<sum(a,b);
+	retrun 0;
+}
