e1327fe2b794c4795849b197b3def257f2ce5d6f
Mayank Gaur <37980122+mykg@users.noreply.github.com>
2020-10-13 16:17:04 +0530

    mayank.cpp having sum xd

---
+++ mayank.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+//write your code here
+return a+b;
+}
+
+int main(){
+	int x,y;
+	cout<<"Enter first number:";
+	cin>>x;
+	cout<<"Enter second number:";
+	cin>>y;
+	cout<<"sum of "<<x<<" and "<<y<<" :"<<sum(a,b);
+	retrun 0;
+}
